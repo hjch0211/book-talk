1372cbecb7d87b7b5b5c593767c55c633921d00d
한재찬 <75880928+hjch0211@users.noreply.github.com>
2025-09-18 23:55:51 +0900

    feat: apply cors setting (#5)

--- book-talk-be/api/src/main/kotlin/kr/co/booktalk/config/PropertiesConfig.kt
+++ book-talk-be/api/src/main/kotlin/kr/co/booktalk/config/PropertiesConfig.kt
@@ -6,5 +6,6 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 @EnableConfigurationProperties(
     LibProperties::class,
+    AppProperties::class,
 )
 class PropertiesConfig
