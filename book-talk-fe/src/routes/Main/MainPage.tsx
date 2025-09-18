525331974c14427aec5ba2f475843807229130dc
Jchan <gkswocks123@gmail.com>
2025-09-18 13:46:33 +0900

    refac: delete legacy ui

--- book-talk-fe/src/routes/Main/MainPage.tsx
+++ book-talk-fe/src/routes/Main/MainPage.tsx
@@ -1,40 +1,10 @@
-import {Header} from '../../components/Header.tsx';
-import {ArticleArea} from '../../components/ArticleArea.tsx';
-import {ChatArea} from '../../components/ChatArea.tsx';
-import {Sidebar} from '../../components/Sidebar.tsx';
-import {useBookTalk} from '../../hooks/useBookTalk.ts';
-import '../../App.css';
+import './MainPage.css';
 
 export function MainPage() {
-    const {
-        article,
-        room,
-        participants,
-        setTitle,
-        addContentBlock,
-        updateContentBlock,
-        deleteContentBlock,
-        handleImageUpload
-    } = useBookTalk();
-
     return (
-        <div className="main-container">
-            <div className="container">
-                <Header room={room}/>
-                <ArticleArea
-                    article={article}
-                    onUpdateBlock={updateContentBlock}
-                    onDeleteBlock={deleteContentBlock}
-                />
-                <ChatArea
-                    currentSpeaker={room.currentSpeaker}
-                    onSendMessage={addContentBlock}
-                    onSendTitle={setTitle}
-                    onUploadImage={handleImageUpload}
-                    hasTitle={!!article.title}
-                />
-            </div>
-            <Sidebar participants={participants}/>
+        <div className="main-page">
+            <main className="main-content">
+            </main>
         </div>
     );
 }
