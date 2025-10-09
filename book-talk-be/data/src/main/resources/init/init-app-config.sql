1ad0509ea30d2982bb2752a74079c8cdd91104d0
Jchan <gkswocks123@gmail.com>
2025-10-09 16:53:59 +0900

    refac: remove unnecessary app config value

--- book-talk-be/data/src/main/resources/init/init-app-config.sql
+++ book-talk-be/data/src/main/resources/init/init-app-config.sql
@@ -1,4 +1,3 @@
 INSERT INTO app_config (key, value, cache_seconds)
-VALUES ('joinDebateDeadlineSeconds', '3600', 60),
-       ('maxDebateMemberCnt', '10', 60),
+VALUES ('maxDebateMemberCnt', '10', 60),
        ('debateRoundSpeakerSeconds', '90', 60);
