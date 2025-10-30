e08b408d7f18ee6a42d365e1ba86bf294aaab89d
Jchan <gkswocks123@gmail.com>
2025-10-31 00:27:20 +0900

    fix: apply Chrome audio autoplay policy (start audio when the user interacts)

--- book-talk-fe/packages/web/src/hooks/useVoiceChat.ts
+++ book-talk-fe/packages/web/src/hooks/useVoiceChat.ts
@@ -1,7 +1,7 @@
 /**
- * useVoiceChat - 간단한 음성 채팅 훅
+ * useVoiceChat - 음성 채팅 훅
  *
- * VoiceChatContext를 그대로 expose
+ * VoiceChatContext의 기능을 컴포넌트에서 사용할 수 있도록 제공합니다.
  */
 
 import { useVoiceChat as useVoiceChatContext } from '../contexts/VoiceChatContext';
