d10153e601b4653428bec4790dbd2abed93a6735
Jchan <gkswocks123@gmail.com>
2025-12-03 14:40:17 +0900

    refac: add index

---
+++ book-talk-fe/packages/web/src/components/index.ts
@@ -0,0 +1,7 @@
+export { AudioPlayer } from './molecules/AudioPlayer';
+export { AudioActivationBanner } from './organisms/AudioActivationBanner';
+export { default as MainHeader } from './organisms/MainHeader';
+export { default as Modal } from './organisms/Modal';
+export { Toast } from './organisms/Toast';
+export { default as MainContainer } from './templates/MainContainer';
+export { default as PageContainer } from './templates/PageContainer';
