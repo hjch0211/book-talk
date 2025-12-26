388d7a9885c0fcf9a877e3d28348b9a383834fa1
Jchan <gkswocks123@gmail.com>
2025-12-26 13:21:29 +0900

    refac: apply ramda props function

--- book-talk-ts/packages/web/src/routes/Debate/_components/RoundStartBackdrop.tsx
+++ book-talk-ts/packages/web/src/routes/Debate/_components/RoundStartBackdrop.tsx
@@ -1,5 +1,6 @@
 import { Backdrop, Box, Typography } from '@mui/material';
 import type { RoundType } from '@src/hooks';
+import * as R from 'ramda';
 import type React from 'react';
 
 interface RoundStartBackdropProps {
@@ -63,41 +64,39 @@ const Description: React.FC<BackdropTextProps> = ({ children }) => (
   </Typography>
 );
 
-const ROUND_CONTENT = {
-  PRESENTATION: {
-    title: '1 라운드 시작',
-    subtitle: '발표지를 활용해 자신의 생각이나 의견을 말해주세요!',
-    description: (
-      <>
-        우측 프로필의 순서대로 발표가 진행됩니다.
-        <br />
-        시간 내 발표가 끝나면 발표끝내기 버튼을 눌러 다음사람에게 넘겨주세요.
-      </>
-    ),
-  },
-  FREE: {
-    title: '2 라운드 시작',
-    subtitle: '다양한 자료를 공유하며 자유롭게 토론해주세요!',
-    description: (
-      <>
-        프로필 메뉴기능을 통해 다른 사람의 발표지를 확인하거나 발언권을 넘겨줄 수 있어요.
-        <br />
-        손들기 버튼으로 발언권을 주장하고 발표자를 넘겨받으세요.
-      </>
-    ),
-  },
-  PREPARATION: {
-    title: '준비 중',
-    subtitle: '토론 시작을 기다리는 중입니다.',
-    description: '호스트가 토론을 시작할 때까지 기다려주세요.',
-  },
-};
-
 /** 라운드 시작 시 표시되는 가이드 백드롭 */
 export function RoundStartBackdrop({ show, roundType, onClose }: RoundStartBackdropProps) {
   if (!show || !roundType) return null;
 
-  const content = ROUND_CONTENT[roundType];
+  const content = R.prop(roundType, {
+    PRESENTATION: {
+      title: '1 라운드 시작',
+      subtitle: '발표지를 활용해 자신의 생각이나 의견을 말해주세요!',
+      description: (
+        <>
+          우측 프로필의 순서대로 발표가 진행됩니다.
+          <br />
+          시간 내 발표가 끝나면 발표끝내기 버튼을 눌러 다음사람에게 넘겨주세요.
+        </>
+      ),
+    },
+    FREE: {
+      title: '2 라운드 시작',
+      subtitle: '다양한 자료를 공유하며 자유롭게 토론해주세요!',
+      description: (
+        <>
+          프로필 메뉴기능을 통해 다른 사람의 발표지를 확인하거나 발언권을 넘겨줄 수 있어요.
+          <br />
+          손들기 버튼으로 발언권을 주장하고 발표자를 넘겨받으세요.
+        </>
+      ),
+    },
+    PREPARATION: {
+      title: '준비 중',
+      subtitle: '토론 시작을 기다리는 중입니다.',
+      description: <>'호스트가 토론을 시작할 때까지 기다려주세요.'</>,
+    },
+  });
 
   return (
     <Backdrop
