6938a8be5b4f8b71a37f7e2d84f4e7bd6cf9f6ad
Jchan <gkswocks123@gmail.com>
2025-12-29 15:57:46 +0900

    feat: add LangGraphNode interface

---
+++ book-talk-ts/packages/ai/src/lang-graph-node.ts
@@ -0,0 +1,9 @@
+/**
+ * LangGraph node interface
+ *
+ * LangGraph node는 반드시 이 인터페이스를 구현해야합니다.
+ */
+export interface LangGraphNode<T> {
+  /** 노드 작업(graph 등록용) */
+  process(state: T): Promise<Partial<T>>;
+}
