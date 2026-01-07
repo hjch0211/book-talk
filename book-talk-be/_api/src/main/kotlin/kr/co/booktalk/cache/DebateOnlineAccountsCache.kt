d7fb2ee732cb1b365fd258b66e36771bf1ad3199
Jchan <gkswocks123@gmail.com>
2026-01-07 17:04:02 +0900

    feat: add DebateOnlineAccountsCache

---
+++ book-talk-be/_api/src/main/kotlin/kr/co/booktalk/cache/DebateOnlineAccountsCache.kt
@@ -0,0 +1,30 @@
+package kr.co.booktalk.cache
+
+import org.springframework.stereotype.Component
+import java.time.Duration
+
+/** 현재 토론방의 접속중인 account cache (debateId: Set<accountId>) */
+@Component
+class DebateOnlineAccountsCache(private val cacheClient: CacheClient) {
+    companion object {
+        private const val PREFIX = "debate:online_accounts:"
+        private val TTL: Duration = Duration.ofDays(1)
+    }
+
+    fun add(debateId: String, accountId: String) {
+        val key = key(debateId)
+        cacheClient.addToSet(key, accountId)
+        cacheClient.expire(key, TTL)
+    }
+
+    fun remove(debateId: String, accountId: String) {
+        cacheClient.removeFromSet(key(debateId), accountId)
+    }
+
+    fun get(debateId: String): Set<String> {
+        return cacheClient.getSetMembers(key(debateId))
+    }
+
+    private fun key(debateId: String): String =
+        PREFIX + debateId
+}
