4afdf325097d7347d050a756d24cd463f578c31c
Jchan <gkswocks123@gmail.com>
2026-05-15 19:17:19 +0900

    Revert "chore(deps): bump jvm from 2.0.0 to 2.3.21 in /book-talk-be (#143)"
    This reverts commit 45f14aaf9a09f2e45977952a7d03aedaafb0a13f.

--- book-talk-be/build.gradle.kts
+++ book-talk-be/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.3.21" apply false
+    kotlin("jvm") version "2.0.0" apply false
     kotlin("plugin.spring") version "2.0.0" apply false
     kotlin("plugin.jpa") version "2.0.0" apply false
     id("org.springframework.boot") version "3.5.4" apply false
